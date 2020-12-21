73d45d521995e6fea74b69ec025d1beb9260771e
Darkhan <darkhan.zhetkergen@mail.ru>
2020-12-21 21:05:02 +0600

    Anime fixed

--- src/components/anime/Anime.tsx
+++ src/components/anime/Anime.tsx
@@ -19,6 +19,7 @@ export default function Anime({}: Props): ReactElement {
             const animes = response.data
             setAnime(animes)
         }
+        getAnimeList();
     }, [])
 
     return (
